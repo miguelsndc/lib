51b0cd74d09134406cc2f82546eb5716ca2932e5
Miguel Nogueira msnc <mglsndc@gmail.com>
2026-03-20 15:36:18 -0300

    Refactor matrix exponentiation code and structure

--- code/math/matrix-exp.cpp
+++ code/math/matrix-exp.cpp
@@ -1,45 +1,29 @@
-/*
-    Exponenciacao Rapida de Matrizes  O(m³ log (b))
-    Calcula recorrências lineares
-*/
-typedef vector<vector<i64>> matrix;
-
-matrix init(int size) {
-    matrix mat(size, vector<i64>(size));
-    return mat;
-}
-
-vector<i64> vecmul(matrix m, vector<i64> vec, i64 sz, i64 mod) {
-    assert(vec.size() == sz);
-    vector<i64> ans;
-    for (int i = 0; i < sz; i++) {
-        for (int j = 0; j < sz; i++) {
-            ans[i] += (vec[j] * (m[i][j]));
-        }
-    }
-    return ans;
-}
-
-matrix matmul(matrix m1, matrix m2, i64 mod, i64 sz) {
-    matrix ans;
-    for (int i = 0; i < sz; i++) {
-        for (int j = 0; j < sz; j++) {
-            for (int k = 0; k < sz; k++) {
-                ans[i][j] = (ans[i][j] + 1LL * (m1[i][k] % mod) * (m2[k][j] % mod)) % mod;
+struct Matrix {
+    int n;
+    vector<vector<i64>> m;
+    Matrix(int n) : n(n), m(n, vector<i64>(n, 0)) {}
+    Matrix mul(const Matrix& other, i64 mod) const {
+        assert(n == other.n);
+        Matrix res(n);
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < n; k++) {
+                if (!m[i][k]) continue;
+                for (int j = 0; j < n; j++) {
+                    res.m[i][j] = (res.m[i][j] + (m[i][k] * other.m[k][j]) % mod) % mod;
+                }
             }
         }
+        return res;
     }
-    return ans;
-}
-
-// O(log(b))
-matrix fexp(matrix p, i64 b, i64 mod, i64 sz) {
-    matrix ans;
-    for(int i = 0; i < sz; i++) ans[i][i] = 1;
-    while(b){
-        if(b & 1) ans = matmul(ans, p, mod, sz);
-        p = matmul(p, p, mod, sz);
-        b >>= 1;
+    Matrix fexp(i64 b, i64 mod) const {
+        Matrix res(n);
+        for (int i = 0; i < n; i++) res.m[i][i] = 1; 
+        Matrix base = *this;
+        while (b > 0) {
+            if (b & 1) res = res.mul(base, mod);
+            base = base.mul(base, mod);
+            b >>= 1;
+        }
+        return res;
     }
-    return ans;
-}
+};
