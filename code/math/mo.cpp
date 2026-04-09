ccab476492115421b64338998eff57334063107b
Miguel Nogueira msnc <mglsndc@gmail.com>
2026-04-09 14:40:35 -0300

    Refactor Mo's algorithm implementation in mo.cpp

--- code/math/mo.cpp
+++ code/math/mo.cpp
@@ -1,77 +1,50 @@
-/*
-    Mo's algorithm
-    Answer OFFLINE range queries in O((n + q) sqrt (n))
-*/
-
-
-// Hilbert order (mo robado)
-
-inline int64_t hilbertOrder(int x, int y, int pow, int rotate){
-    if(pow == 0) return 0;
+#include <bits/stdc++.h>
+using namespace std;
+const int BLOCK = 350; // ajuste para ~sqrt(N)
+inline int64_t hilbertOrder(int x, int y, int pow, int rotate) {
+    if (pow == 0) return 0;
     int hpow = 1 << (pow - 1);
-    int seg = (x < hpow) ? ( (y < hpow) ? 0 : 3) : ( (y < hpow) ? 1 : 2);
+    int seg = (x < hpow) ? ((y < hpow) ? 0 : 3) : ((y < hpow) ? 1 : 2);
     seg = (seg + rotate) & 3;
     const int rotateDelta[4] = {3, 0, 0, 1};
     int nx = x & (x ^ hpow), ny = y & (y ^ hpow);
     int nrot = (rotate + rotateDelta[seg]) & 3;
-    int64_t subSquareSize = int64_t(1) << (2*pow - 2);
+    int64_t subSquareSize = 1LL << (2 * pow - 2);
     int64_t ans = seg * subSquareSize;
     int64_t add = hilbertOrder(nx, ny, pow - 1, nrot);
-    ans += (seg == 1 || seg == 2) ? add : (subSquareSize - add - 1);
-    return ans;
+    return ans + ((seg == 1 || seg == 2) ? add : (subSquareSize - add - 1));
 }
-struct Query{
-    int l, r, idx;
-    int64_t ord;
-    Query(int l, int r, int idx) : l(l), r(r), idx(idx) {
-        ord = hilbertOrder(l, r, 21, 0);
-    }
-    bool operator < (Query &other){
-        return ord < other.ord;
-    }
-};
 
-
-int len;  // roughly sqrt n
 struct Query {
-    int l, r, idx;
-    bool operator<(const Query& other) const {
-        int block_a = l / len, block_b = other.l / len;
-        if (block_a != block_b)
-            return block_a < block_b;
-        return (block_a & 1) ? (r > other.r) : (r < other.r);
+    int l, r, id;
+    int64_t ord; // use se hilbert
+    inline void calc_hilbert() { ord = hilbertOrder(l, r, 21, 0); } // use se hilbert
+    bool operator<(const Query& o) const {
+        int b1 = l / BLOCK, b2 = o.l / BLOCK;
+        if (b1 != b2) return b1 < b2;
+        return (b1 & 1) ? (r < o.r) : (r > o.r); 
     }
 };
 
-int get_ans() {
-}
-void add(int idx) {
-}
-void remove(int idx) {
-}
+void add(int i) {} 
+void remove(int i)
+int get_ans()
 
-template <typename T>
-void mo(vector<Query> queries) {
-    sort(all(queries));
-    ans.assign(queries.size(), 0);
-    int cur_l = 0, cur_r = -1;
-    for (Query q : queries) {
-        while (cur_l > q.l) {
-            cur_l--;
-            add(cur_l);
-        }
-        while (cur_r < q.r) {
-            cur_r++;
-            add(cur_r);
-        }
-        while (cur_l < q.l) {
-            remove(cur_l);
-            cur_l++;
-        }
-        while (cur_r > q.r) {
-            remove(cur_r);
-            cur_r--;
-        }
-        ans[q.idx] = get_ans();  // get answer
+void mo(vector<Query>& queries) {
+    // for (auto& q : queries) q.calc_hilbert();
+    // sort(queries.begin(), queries.end(), [](const Query& a, const Query& b) {
+    //     return a.ord < b.ord;
+    // });
+
+    sort(queries.begin(), queries.end());
+    
+    int L = 0, R = -1; 
+    for (auto& q : queries) {
+        while (L > q.l) add(--L);
+        while (R < q.r) add(++R);
+        while (L < q.l) remove(L++);
+        while (R > q.r) remove(R--);
+        
+        ans[q.id] = get_ans();
     }
 }
