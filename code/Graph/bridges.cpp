652405824844a13f51c92a3c827e5dd40ec69895
Miguel Nogueira msnc <mglsndc@gmail.com>
2026-06-03 18:30:07 -0300

    Improve bridge detection and build bridge tree
    Refactor bridge-finding algorithm and add tree construction

--- code/Graph/bridges.cpp
+++ code/Graph/bridges.cpp
@@ -1,26 +1,48 @@
-/*
-    Acha todas as bridges em O(N + M)
-*/
+vector<int> g[ms], bridge_tree[ms];
+int dfn[ms], low[ms], timer = 0, vis[ms];
+set<pair<int,int>> pontes;
+void acha_pontes(int u, int p = -1) {
+    dfn[u] = low[u] = timer++;
+    for (int v : g[u]) {
+        if (v == p) continue;
+        if (dfn[v] == -1) {
+            acha_pontes(v);
+            low[u] = min(low[u], low[v]);
+            if (low[v] > dfn[u]) {
+                // (u,v) é uma bridge
+                //process(u,v);
+                pontes.insert(minmax(u,v));
+            }
+        } else {
+            low[u] = min(low[u], dfn[v]);
+        }
+    }
+}
 
-int n, m;
-const int mxn = 1e5 + 5;
-vector<int> g[mxn];
-int tin[mxn], low[mxn];
-vector<pii> bridges;
-int timer = 1;
-
-void dfs(int u, int p) {
-    tin[u] = timer++;
-    low[u] = tin[u];
-    int ch = 0;
-    for (int v : g[u])
-        if (v != p) {
-            if (tin[v])  // lowlink direta
-                low[u] = min(tin[v], low[u]);
-            else {
-                dfs(v, u);
-                low[u] = min(low[v], low[u]);
-                if (tin[u] < low[v]) bridges.push_back({u, v});
+void build_tree(int n) {
+    int cc = 0;
+    // assumindo q o grafo é conexo e nao-ponderado
+    acha_pontes(0);
+    vector<int> component(n);
+    auto dfs = [&] (auto &&self, int u, int num)->void {
+        component[u]=num;
+        for (int v: g[u]) {
+            if (!pontes.count(minmax(u,v)) and !vis[v]) {
+                self(self, v, num);
             }
         }
+    };
+    for (int u=0;u<n;u++) {
+        if (!vis[u]) dfs(dfs, u, cc++);
+    }
+    for (auto &[u,v]:pontes) {
+        bridge_tree[component[u]].push_back(component[v]);
+        bridge_tree[component[v]].push_back(component[u]);
+    }
 }
+
+/*
+    O(n + m log m) pra construir a bridge tree
+    tambem é possivel construir em O(n+m) se guardar o id da aresta
+    e setar ponte[aresta]=true pra toda ponte
+*/
