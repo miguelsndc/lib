f3a2238e656fca39d962a3cd26ff514876a67a10
Miguel Nogueira msnc <mglsndc@gmail.com>
2026-06-25 11:01:01 -0300

    Refactor hashing implementation and structure

--- code/String/hash.cpp
+++ code/String/hash.cpp
@@ -1,39 +1,26 @@
-/*
-    Double Polynomial Hashing
-    Prehcalculo - O(n)
-    Substring hash queries - O(1)
-    Hash(l, m - 1) calcula o hash da substring incluindo o l de tamanho m
-*/
+const int MAXN = 2e5 + 5; 
+const long long M1 = 188888881, M2 = 1e9 + 7, B = 137;
+long long p1[MAXN], p2[MAXN];
 
-const int MOD1 = 188'888'881;
-const int MOD2 = 1e9 + 7;
-const int base = 137;
-
-ll pow1[MAXN];
-ll pow2[MAXN];
-
-// O(n) - Chamar antes
 void calc_pow() {
-    pow1[0] = pow2[0] = 1;
-    for (int i = 1; i < MAXN; i++)
-        pow1[i] = (pow1[i - 1] * base) % MOD1,
-        pow2[i] = (pow2[i - 1] * base) % MOD2;
+    p1[0] = p2[0] = 1;
+    for (int i = 1; i < MAXN; i++) {
+        p1[i] = p1[i - 1] * B % M1;
+        p2[i] = p2[i - 1] * B % M2;
+    }
 }
 
-struct Hashing {
-    vector<pair<ll, ll>> pref;
-    // O(1)
-    Hashing(string &s) {
-        pref = vector<pair<ll, ll>>(s.size() + 1, {0, 0});
-        for (int i = 0; i < s.size(); i++)
-            pref[i + 1].first = ((pref[i].first * base) % MOD1 + s[i]) % MOD1,
-                     pref[i + 1].second = ((pref[i].second * base) % MOD2 + s[i]) % MOD2;
+struct Hash {
+    vector<long long> h1, h2;
+    Hash(string& s) : h1(s.size() + 1), h2(s.size() + 1) {
+        for (int i = 0; i < s.size(); i++) {
+            h1[i + 1] = (h1[i] * B + s[i]) % M1;
+            h2[i + 1] = (h2[i] * B + s[i]) % M2;
+        }
     }
-
-    // O(1)
-    ll operator()(int a, int b) {
-        ll h1 = (MOD1 + pref[b + 1].first - (pref[a].first * pow1[b - a + 1]) % MOD1) % MOD1;
-        ll h2 = (MOD2 + pref[b + 1].second - (pref[a].second * pow2[b - a + 1]) % MOD2) % MOD2;
-        return (h1 << 32) | h2;
+    long long get(int l, int r) {
+        long long a = (M1 + h1[r + 1] - h1[l] * p1[r - l + 1] % M1) % M1;
+        long long b = (M2 + h2[r + 1] - h2[l] * p2[r - l + 1] % M2) % M2;
+        return (a << 32) | b;
     }
 };
