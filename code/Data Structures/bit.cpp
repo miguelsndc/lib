bb1290e6dac4bd9133842ebb92a0587569a01e35
Miguel Nogueira msnc <mglsndc@gmail.com>
2026-07-07 09:27:19 -0300

    Refactor BIT structure for improved readability

--- code/Data Structures/bit.cpp
+++ code/Data Structures/bit.cpp
@@ -1,20 +1,6 @@
-struct BIT{
-    int n;
-    vector<ll> bit;
-    BIT(int _n = 0) { init(_n); }
-    void init(int _n) {
-        n = _n;
-        bit.assign(n + 1, 0);
-    }
-    // add val at position pos (0-based)
-    void add(int pos, int val = 1) {
-        for (int i = pos + 1; i <= n; i += i & -i) bit[i] += val;
-    }
-    // sum [0..pos] (0-based)
-    ll sum(int pos) const {
-        if (pos < 0) return 0;
-        ll s = 0;
-        for (int i = pos + 1; i > 0; i -= i & -i) s += bit[i];
-        return s;
-    }
+struct BIT {
+    int n; vector<ll> bit;
+    BIT(int n = 0) : n(n), bit(n + 1, 0) {}
+    void add(int i, int v = 1) { for (++i; i <= n; i += i & -i) bit[i] += v; }
+    ll sum(int i) const { ll s = 0; for (++i; i > 0; i -= i & -i) s += bit[i]; return s; }
 };
